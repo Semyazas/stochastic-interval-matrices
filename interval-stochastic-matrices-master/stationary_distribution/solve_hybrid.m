cca33c240514b6179d34f6c888df8abb936aa0fd
Martin Kunst <martin.kunst2001@centrum.cz>
2024-12-18 18:15:22 +0100

    feature: implementation of hybrid mehod, further debugging neede d

--- interval-stochastic-matrices-master/stationary_distribution/solve_hybrid.m
+++ interval-stochastic-matrices-master/stationary_distribution/solve_hybrid.m
@@ -6,29 +6,26 @@
 
 function x = solve_hybrid(interval_m)
     correct_dimensions(interval_m);
+    upper_m1 = inf(interval_m);
 
     parametric_system = generate_parametric_system(interval_m);
 
     system_temp = interval_m - eye(size(inf(interval_m),1));
-    non_parametric_system = [ system_temp ; 
-               ones(1,size(inf(interval_m),1))
-             ];
+    non_parametric_system = system_temp;
 
     % Pad the non-parametric system with zeros to match the number of columns in the parametric system
-    [rows_p, cols_p] = size(parametric_system);
-    [rows_np, ~] = size(non_parametric_system);   
+    [rows_p, cols_p]   = size(parametric_system);
+    [rows_np, cols_np] = size(non_parametric_system);   
 
     
-    padded_non_parametric_system = [non_parametric_system, zeros(rows_np, cols_p)];
+    padded_non_parametric_system = [non_parametric_system, zeros(rows_np, cols_p - cols_np)];
     
     % Concatenate parametric and padded non-parametric systems
 
-    print(padded_non_parametric_system)
-    print(parametric_system)
+    disp(size(padded_non_parametric_system))
+    disp(size(parametric_system))
     system = [padded_non_parametric_system;
               parametric_system];
-
-    upper_m1 = sup(parametric_system);
 
     % Calculate the center and radius of the interval system
     radius = rad(system);
@@ -43,15 +40,16 @@ function x = solve_hybrid(interval_m)
 
     vars_count = size(inf(interval_m), 1);
     b2 = zeros(1, vars_count);
-    b2 = [b2 1];
-    b2 = [b2 -b2 zeros(1, vars_count)];
+    b2 = [b2 -b2 ];
     
-
+    disp(b2)
+    disp(non_parametric_system)
     % Solve the system
-    x = solve(size(lower_m, 1), center, radius, [b1 b2]);
+    x = solve(size(inf(interval_m),1), center, ...
+        radius, [b2 b1],size(padded_non_parametric_system,2));
 end
 
-function x = solve(vars_count, center, mg, b)
+function x = solve(vars_count, center, mg, b, cols_par)
     options = optimoptions('linprog', 'Display', 'none');
     x_inf = zeros(1, vars_count);
     x_sup = ones(1, vars_count);
@@ -60,27 +58,30 @@ function x = solve(vars_count, center, mg, b)
     for i=1:vars_count
        B((vars_count)*(i-1) + i,vars_count*(i-1) + i) = 1;
     end
-
-    A = [-eye(vars_count), zeros(vars_count, size(mg, 2) - vars_count);
+    
+    A = [-eye(vars_count), zeros(vars_count, cols_par - vars_count);
              zeros(vars_count*vars_count,vars_count),zeros(vars_count*vars_count),B;
              zeros(vars_count*vars_count,vars_count),-eye(vars_count*vars_count),zeros(vars_count*vars_count)];
-        
+    
     b = [b , zeros(1,vars_count*vars_count+vars_count*vars_count)];
 
     for i = 1:vars_count
         f1 = zeros(1, size(center, 2));
         f1(i) = 1;  
         constraints_ineq = [center - mg; -center - mg; A];
+        disp("consraint:")
+        disp(size(constraints_ineq))
+        disp(size(b))
 
         constraints_ineq = sparse(constraints_ineq);
 
         [~, fval_inf] = linprog(f1, constraints_ineq, b, [], [], [], [], options);
         [~, fval_sup] = linprog(-f1, constraints_ineq, b, [], [], [], [], options);
-    %    disp(a);
-    %    disp(b);
+        disp(fval_sup);
+        disp(fval_inf);
         x_sup(i) = -fval_sup;
         x_inf(i) = fval_inf;
     end
     x = infsup(x_inf, x_sup);
-   % disp(x);
+    % disp(x);
 end
