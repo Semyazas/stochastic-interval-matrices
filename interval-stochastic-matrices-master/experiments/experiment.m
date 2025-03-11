90c271a9567655805c75af4eaf4fe659a3d5e79c
Martin Kunst <martin.kunst2001@centrum.cz>
2025-03-11 11:46:14 +0100

    feat(added generator of interval matrices as parameter to experiments: TODO: look at intersection print)

--- interval-stochastic-matrices-master/experiments/experiment.m
+++ interval-stochastic-matrices-master/experiments/experiment.m
@@ -7,34 +7,42 @@
 %   error - A value indicating how "wide" the generated matrices are in the experiment. 
 %           Random irreducible stochastic matrices A are generated, and then 
 %           interval stochastic matrices [A] are created as [(1-error)A, (1+error)A].
+%   generate_random_IISM - function, that can generate IISM = Interval
+%           irreducible stochastic matrix
 %
 % The function displays the results of the experiments, including the average 
 % ratios and times for the parametric and non-parametric methods.
-function experiment(dimensions, number_of_iterations,error)
+function experiment(dimensions, number_of_iterations,error, generate_random_IISM)
     options = optimset('linprog');
     options.Display = 'off';
 
     % we test for each dimension
-
-    for i=1:size(dimensions,2)
+    disp(size(dimensions))
+    disp("aa")
+    for i=1:length(dimensions)
+        disp(size(dimensions))
         disp(dimensions(i));
-        small_experiment(dimensions(i),number_of_iterations,error);
+        small_experiment(dimensions(i),number_of_iterations,error, generate_random_IISM);
         disp("");
         disp("---------------------------------------------------");
         disp("");
     end
 end
+
+
 % This helper function performs a small experiment for a specific dimension.
 %
 % Inputs:
 %   dimension - An integer specifying the dimension of the matrix.
 %   number_of_iterations - An integer specifying the number of iterations for testing.
 %   error - A value indicating the width of the generated matrices in the experiment.
+%    generate_random_IISM - function, that can generate IISM = Interval
+%    irreducible stochastic matrix
 %
 % Outputs:
 %   average_ratio - The average ratio of the parametric method to the non-parametric method.
 %   average_time - The average time taken by each method.
-function [average_ratio] = small_experiment(dimension, number_of_iterations,error)
+function [average_ratio] = small_experiment(dimension, number_of_iterations,error, generate_random_IISM)
     average_ratio = 0;
     average_time_parametric = 0;
     average_time_non_parametric = 0;
@@ -42,7 +50,7 @@ function [average_ratio] = small_experiment(dimension, number_of_iterations,erro
     average_ratio_hybrid = 0;
     average_time_hybrid = 0;
     for i=1:number_of_iterations
-        [lower,upper] = rand_int_irreduc_stoch_matrix(dimension,error);
+        [lower,upper] = generate_random_IISM(dimension,error);
        
         tic
         x_non_parametric = solve_non_parametric(...
@@ -71,7 +79,6 @@ function [average_ratio] = small_experiment(dimension, number_of_iterations,erro
     average_ratio_intersection_non_par = average_ratio_intersection_non_par / number_of_iterations;
     average_ratio = average_ratio / number_of_iterations;
     average_ratio_hybrid = average_ratio_hybrid / number_of_iterations;
-    
 
     disp("average ratio:               " + average_ratio);
     disp("average ratio hybrid:        " + average_ratio_hybrid);
